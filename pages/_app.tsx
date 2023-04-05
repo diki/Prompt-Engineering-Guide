c1846bf4e8af6d0eb01d512882f5b301db36ace5
Mustafa Dikici <musdikici@gmail.com>
2023-04-05 09:25:55 +0200

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,26 @@
-import type { AppProps } from 'next/app';
-import Script from 'next/script';
-import { Analytics } from '@vercel/analytics/react';
- 
+import type { AppProps } from "next/app";
+import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-HKFRG01NR4"
+      />
+      <Script>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-HKFRG01NR4');
+      `}
+      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
   );
 }
- 
+
 export default MyApp;
